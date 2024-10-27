647140e589978d10fc37e1b3391409bc6804ff38
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-10-27 14:08:09 -0400

    window switching binds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,6 +9,12 @@ vim.opt.signcolumn = 'yes'
 -- colorscheme
 vim.opt.termguicolors = true
 
+-- window switching
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
