ce9c69a836834b476117be8f67f23f0e9302572c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-10-26 23:57:00 -0400

    move lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.signcolumn = 'yes'
 
-
+-- colorscheme
+vim.opt.termguicolors = true
+--
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -23,20 +25,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
+--
 -- PLUGINS
 require("lazy").setup({
   spec = {
-		{
-			"folke/tokyonight.nvim",
-			lazy = false,
-			priority = 1000,
-			opts = {},
-		},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-	  {'neovim/nvim-lspconfig'},
-		{'williamboman/mason.nvim'},
-		{'williamboman/mason-lspconfig.nvim'},
+		{import = "plugins"},
 	  {'hrsh7th/cmp-nvim-lsp'},
 	  {'hrsh7th/nvim-cmp'},
 		{'m4xshen/autoclose.nvim'},
@@ -50,50 +43,6 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
 })
 
--- colorscheme
-vim.opt.termguicolors = true
-vim.cmd([[colorscheme tokyonight]])
-
--- Add cmp_nvim_lsp capabilities settings to lspconfig
--- This should be executed before you configure any language server
-local lspconfig_defaults = require('lspconfig').util.default_config
-lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
--- LSP keymaps
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
-})
-
--- LSP installer
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	ensure_installed = {"lua_ls", "clangd", "pyright"},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
-})
-
-require('lspconfig').dafny.setup({})
 
 -- autocomplete
 local cmp = require('cmp')
@@ -108,7 +57,7 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
 
     -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<cr>'] = cmp.mapping.confirm({select = false}),
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
