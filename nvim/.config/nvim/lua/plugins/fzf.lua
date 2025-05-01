e2d4e0f27108c71596ce2dcb5c72bf46e8cad1b5
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-01 11:10:45 -0400

    todo comment

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -2,7 +2,8 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
 
-  opts = function ()
+  -- needs some work for choosing what to ignore and allowing more search functionality
+  opts = function()
     return {
       files = {
         hidden = false
@@ -12,10 +13,12 @@ return {
 
   keys = function ()
     local fzf = require("fzf-lua")
+
     return {
       {'<leader>ff', fzf.files},
       {'<leader>fs', fzf.lsp_document_symbols},
       {'<leader>ft', fzf.treesitter},
+      {'<leader>fg', fzf.git_files},
     }
   end,
 }
