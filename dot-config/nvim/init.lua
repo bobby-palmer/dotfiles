32534d0b16137b824486bc87ac77ba79755f7ad8
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-13 01:18:24 -0400

    match theme

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -50,7 +50,7 @@ require("lazy").setup({
       lazy = false,
       priority = 1000,
       config = function()
-        vim.cmd([[colorscheme tokyonight]])
+        vim.cmd([[colorscheme tokyonight-night]])
       end
     },
     {
