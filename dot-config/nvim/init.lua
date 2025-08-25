9fbeee7feb33a12e80fca514883395b337133b5b
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-08-25 10:48:57 -0400

    mini

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -7,7 +7,6 @@ keymap("n", "<C-l>", "<C-w><C-l>")
 keymap("n", "<C-h>", "<C-w><C-h>")
 keymap("n", "<C-j>", "<C-w><C-j>")
 keymap("n", "<C-k>", "<C-w><C-k>")
-
 keymap("n", "j", "gj")
 keymap("n", "k", "gk")
 
@@ -30,7 +29,7 @@ vim.lsp.enable({
   'gopls'
 })
 
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({ virtual_text = true }) -- TODO
 
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -60,18 +59,6 @@ require("lazy").setup({
       config = function()
         vim.cmd([[colorscheme tokyonight-night]])
       end
-    },
-    {
-      "ibhagwan/fzf-lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      keys = function ()
-        local fzf = require("fzf-lua")
-        return {
-          {'<leader>ff', fzf.files},
-          {'<leader>fs', fzf.lsp_document_symbols},
-          {'<leader>ft', fzf.treesitter},
-        }
-      end,
     },
     {
       'stevearc/oil.nvim',
@@ -104,57 +91,13 @@ require("lazy").setup({
         })
       end
     },
-    {
-      'saghen/blink.cmp',
-      version = '1.*',
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        keymap = {
-          preset = 'super-tab',
-        },
-        appearance = { nerd_font_variant = 'mono' },
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-        completion = {
-          list = {
-            selection = { preselect = false }
-          }
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    },
-    {
-      'saghen/blink.pairs',
-      version = '*',
-      dependencies = 'saghen/blink.download',
-      --- @module 'blink.pairs'
-      --- @type blink.pairs.Config
-      opts = {
-        mappings = {
-          enabled = true,
-          pairs = {},
-        },
-        highlights = {
-          enabled = true,
-          groups = {
-            'BlinkPairsOrange',
-            'BlinkPairsPurple',
-            'BlinkPairsBlue',
-          },
-          matchparen = {
-            enabled = true,
-            group = 'MatchParen',
-          },
-        },
-        debug = false,
-      }
-    },
-    -- for default configs
-    { 'neovim/nvim-lspconfig' }
+    { 'echasnovski/mini.nvim', version = false },
+    { 'neovim/nvim-lspconfig' },
   },
 
   install = { colorscheme = { "habamax" } },
 })
+
+require('mini.pairs').setup()
+require('mini.completion').setup()
+require('mini.pick').setup()
