c374e1de42eee51739b20add5917ee2f21a2d391
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-08-26 14:37:55 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -110,10 +110,25 @@ require("lazy").setup({
       ---@type oil.SetupOpts
       opts = {
         keymaps = {
-          ["l"] = {"actions.select", mode = "n"},
-          ["h"] = {"actions.parent", mode = "n"},
+          -- ["g?"] = { "actions.show_help", mode = "n" },
+          -- ["<CR>"] = "actions.select",
+          -- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+          -- ["<C-t>"] = { "actions.select", opts = { tab = true } },
+          -- ["<C-p>"] = "actions.preview",
+          -- ["<C-c>"] = { "actions.close", mode = "n" },
+          -- ["-"] = { "actions.parent", mode = "n" },
+          -- ["_"] = { "actions.open_cwd", mode = "n" },
+          -- ["`"] = { "actions.cd", mode = "n" },
+          -- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+          -- ["gs"] = { "actions.change_sort", mode = "n" },
+          -- ["gx"] = "actions.open_external",
+          -- ["g."] = { "actions.toggle_hidden", mode = "n" },
+          -- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+
+          -- Disable keymaps for window switching
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
         },
-        use_default_keymaps = false,
       },
       keys = {{"-", cmd("Oil")}},
       dependencies = { { "echasnovski/mini.icons", opts = {} } },
