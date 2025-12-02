b92f3f47892d1466a7066f71bf0e0c759b254926
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-12-02 10:06:16 -0500

    we on pack

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,48 +1,43 @@
 local g = vim.g
 
-g.mapleader = ' '
-g.maplocalleader = ' '
+g.mapleader = " "
+g.maplocalleader = " "
 
 local opt = vim.opt
 
+opt.clipboard = "unnamedplus"
 opt.expandtab = true
 opt.number = true
 opt.relativenumber = true
 opt.scrolloff = 5
 opt.shiftwidth = 2
-opt.signcolumn = 'yes'
+opt.signcolumn = "yes"
 opt.softtabstop = 2
 opt.tabstop = 2
 opt.termguicolors = true
+opt.winborder = "rounded"
 
 local keymap = vim.keymap.set
-
----@param command string vim command to execute
----@return function a function that will execute the input command when called
-local cmd = function (command)
-  return function ()
-    vim.cmd(command)
-  end
-end
 
 keymap("n", "<C-l>", "<C-w><C-l>")
 keymap("n", "<C-h>", "<C-w><C-h>")
 keymap("n", "<C-j>", "<C-w><C-j>")
 keymap("n", "<C-k>", "<C-w><C-k>")
 
-keymap("n", "j", "gj")
-keymap("n", "k", "gk")
+keymap("n", "-", "<cmd>Oil<CR>")
+
+keymap("n", "<leader>ff", "<cmd>FzfLua files<CR>")
 
 vim.lsp.enable({
-  'lua_ls',
-  'clangd',
-  'rust_analyzer',
-  'zls',
-  'ts_ls',
-  'pyright',
-  'gopls',
-  'tinymist',
-  'ocamllsp',
+  "lua_ls",
+  "clangd",
+  "rust_analyzer",
+  "zls",
+  "ts_ls",
+  "pyright",
+  "gopls",
+  "ocamllsp",
+  "tinymist",
 })
 
 vim.lsp.config["tinymist"] = {
@@ -51,102 +46,54 @@ vim.lsp.config["tinymist"] = {
     settings = {},
 }
 
--- Slowly shift plugins to here when this works (Oil is cooked)
 vim.pack.add({
-  { src = "https://github.com/folke/tokyonight.nvim" },
-  { src = "https://github.com/echasnovski/mini.icons" },
-  -- { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-})
-
-vim.cmd "colorscheme tokyonight-night"
-
-require 'mini.icons'.setup {}
-
--- require'nvim-treesitter.configs'.setup {}
-
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none",
-    "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    {
-      'stevearc/oil.nvim',
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      opts = {
-        keymaps = {
-          ["<C-h>"] = false,
-          ["<C-l>"] = false,
-        },
-      },
-      keys = {{"-", cmd("Oil")}},
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      lazy = false,
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function ()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-          ensure_installed = {
-            "c",
-            "cpp",
-            "markdown",
-            "markdown_inline"
-          },
-          sync_install = false,
-          highlight = {enable = true},
-          indent = {enable = true},
-        })
-      end
-    },
-    {
-      'saghen/blink.cmp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
-      version = '1.*',
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        keymap = { preset = 'super-tab' },
-        signature = { enabled = true },
-      },
-    },
-    {
-      'chomosuke/typst-preview.nvim',
-      lazy = false,
-      version = '1.*',
-      opts = {},
-    },
-    {
-      "ibhagwan/fzf-lua",
-      dependencies = { "echasnovski/mini.icons" },
-      opts = {},
-      keys = {
-        {"<leader>ff", cmd("FzfLua files")},
-        {"<leader>fg", cmd("FzfLua lgrep_curbuf")},
-      },
-      lazy = false,
-    },
-    { 'neovim/nvim-lspconfig' },
-    { 'echasnovski/mini.pairs', version = false, opts = {} },
-    { 'echasnovski/mini.comment', version = false, opts = {} },
+  "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/echasnovski/mini.icons",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/chomosuke/typst-preview.nvim",
+  {
+    src = "https://github.com/saghen/blink.cmp",
+    version = vim.version.range("^1") -- bundle prebuilt binary
   },
-  install = { colorscheme = { "habamax" } },
+  "https://github.com/echasnovski/mini.pairs",
+  "https://github.com/echasnovski/mini.comment",
+  "https://github.com/ibhagwan/fzf-lua",
 })
+
+vim.cmd("colorscheme tokyonight-night")
+
+require("mini.icons").setup {}
+
+require("oil").setup {
+  keymaps = {
+    ["<C-h>"] = false,
+    ["<C-l>"] = false,
+  }
+}
+
+require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "c",
+    "cpp",
+    "markdown",
+    "markdown_inline"
+  },
+  sync_install = false,
+  highlight = {enable = true},
+  indent = {enable = true},
+}
+
+require("typst-preview").setup {}
+
+require("blink.cmp").setup {
+  keymap = { preset = "super-tab" },
+  signature = { enabled = true },
+}
+
+require("mini.pairs").setup {}
+
+require("mini.comment").setup {}
+
+require("fzf-lua").setup {}
