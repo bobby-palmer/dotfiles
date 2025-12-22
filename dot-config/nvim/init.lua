be50ae5f999f48d2997b89eeb19c7ef3f21e5519
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-12-22 10:13:23 -0500

    cleaner one file

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -6,27 +6,30 @@ g.maplocalleader = " "
 local opt = vim.opt
 
 opt.clipboard = "unnamedplus"
-opt.expandtab = true
+
 opt.number = true
 opt.relativenumber = true
-opt.scrolloff = 5
+
+opt.expandtab = true
 opt.shiftwidth = 2
-opt.signcolumn = "yes"
 opt.softtabstop = 2
 opt.tabstop = 2
+
+opt.signcolumn = "yes"
+
+opt.scrolloff = 5
+
 opt.termguicolors = true
+
 opt.winborder = "rounded"
 
 local keymap = vim.keymap.set
 
+-- Better window switching!
 keymap("n", "<C-l>", "<C-w><C-l>")
 keymap("n", "<C-h>", "<C-w><C-h>")
 keymap("n", "<C-j>", "<C-w><C-j>")
 keymap("n", "<C-k>", "<C-w><C-k>")
-
-keymap("n", "-", "<cmd>Oil<CR>")
-
-keymap("n", "<leader>ff", "<cmd>FzfLua files<CR>")
 
 vim.lsp.enable({
   "lua_ls",
@@ -39,32 +42,31 @@ vim.lsp.enable({
   "ocamllsp",
 })
 
-vim.pack.add({
-  "https://github.com/folke/tokyonight.nvim",
-  "https://github.com/echasnovski/mini.icons",
-  "https://github.com/stevearc/oil.nvim",
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/neovim/nvim-lspconfig",
-  {
-    src = "https://github.com/saghen/blink.cmp",
-    version = vim.version.range("^1") -- bundle prebuilt binary
-  },
-  "https://github.com/echasnovski/mini.pairs",
-  "https://github.com/echasnovski/mini.comment",
-  "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/nvim-lualine/lualine.nvim",
-})
-
+-- Set colorscheme
+vim.pack.add {'https://github.com/folke/tokyonight.nvim'}
 vim.cmd("colorscheme tokyonight-night")
 
+-- Add default LSP configurations (maybe write my own later)
+vim.pack.add {'https://github.com/neovim/nvim-lspconfig'}
+
+-- Nicer icons
+vim.pack.add {'https://github.com/echasnovski/mini.icons'}
 require("mini.icons").setup {}
+
+-- Filesystem editing plugin
+vim.pack.add {'https://github.com/stevearc/oil.nvim'}
 
 require("oil").setup {
   keymaps = {
-    ["<C-h>"] = false,
-    ["<C-l>"] = false,
+    ["<C-h>"] = false, -- Disable for window switching
+    ["<C-l>"] = false, -- Ditto
   }
 }
+
+keymap("n", "-", "<cmd>Oil<CR>") -- Vim vinegar keybind
+
+-- Treesitter
+vim.pack.add {'https://github.com/nvim-treesitter/nvim-treesitter'}
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = {
@@ -79,15 +81,32 @@ require("nvim-treesitter.configs").setup {
   indent = {enable = true},
 }
 
+-- Autopair charaters ie (){}[]
+vim.pack.add {'https://github.com/echasnovski/mini.pairs'}
+require("mini.pairs").setup {}
+
+-- Comment toggling
+vim.pack.add {'https://github.com/echasnovski/mini.comment'}
+require("mini.comment").setup {}
+
+-- Nice status line
+vim.pack.add {'https://github.com/nvim-lualine/lualine.nvim'}
+require("lualine").setup {}
+
+-- Fuzzy finder
+vim.pack.add {'https://github.com/ibhagwan/fzf-lua'}
+require("fzf-lua").setup {}
+keymap("n", "<leader>ff", "<cmd>FzfLua files<CR>")
+
+-- Autocomplete
+vim.pack.add({
+  {
+    src = "https://github.com/saghen/blink.cmp",
+    version = vim.version.range("^1") -- bundle prebuilt binary
+  },
+})
+
 require("blink.cmp").setup {
   keymap = { preset = "super-tab" },
   signature = { enabled = true },
 }
-
-require("mini.pairs").setup {}
-
-require("mini.comment").setup {}
-
-require("fzf-lua").setup {}
-
-require("lualine").setup {}
