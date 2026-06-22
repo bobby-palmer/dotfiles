0e7f7f44887f97736d92fec8f9b45c697ba53018
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2026-06-22 14:26:25 -0400

    nvim from first principals

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,7 +1,18 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('options')
-require('plugins')
-require('keymaps')
-require('lsp')
+local opt = vim.opt
+
+-- nav + lines
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.wrap = false
+
+-- indents
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.autoindent = true
--- dot-config/nvim/lua/keymaps.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.keymap.set
-
--- better window switching
-map('n', '<C-h>', '<C-w>h')
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
